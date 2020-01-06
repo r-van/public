0d086688aa76ddf41be5a066fc9c0e85efd0e57d
r-van <59544482+r-van@users.noreply.github.com>
2020-01-05 18:25:39 -0600

    Update aircrack-mc.lua

--- opencomputers/aircrack-mc.lua
+++ opencomputers/aircrack-mc.lua
@@ -1,3 +1,5 @@
+-- Wireless port scanner, taking advantage of the network driver arp-style host resolution protocol
+
 local modem = require("component").modem
 local event = require "event"
 
@@ -9,7 +11,9 @@ print("\naircrack-mc 1.0\n")
 print("BSSID                                   PORT")
 
 for i=1,65535 do
+  -- broadcast a host announce
   modem.broadcast(i, "H")
+  -- at 0.25, this is way too slow, but still trying to find the sweet spot
   response, laddress, raddress = event.pull(0.25, "modem")
   if response then
     print(raddress, i)
